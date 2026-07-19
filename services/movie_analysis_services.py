06b7517f516326291752c63f638ebd8fe4933914
Arlan <arlan.marat.kz@gmail.com>
2026-07-19 21:00:41 +0500

    added services for work with movies such as add movie , delete , update , etc.

---
+++ services/movie_analysis_services.py
@@ -0,0 +1,30 @@
+from database import session
+from models import Movie
+from sqlalchemy import func
+
+class MovieAnalysis:
+    def avg_rate(self):
+        average = session.query(func.avg(Movie.rating)).scalar()
+        return average
+    def best_movie(self):
+        best_movie = session.query(Movie)\
+            .order_by(Movie.rating.desc())\
+                .first()
+        return best_movie
+    def worst_movie(self):
+        worst_movie = session.query(Movie)\
+            .order_by(Movie.rating)\
+            .first()
+    def watched_count(self):
+        watched = session.query(Movie)\
+            .filter_by(is_watched=True)\
+            .count()
+        return watched
+    def unwatched_count(self):
+        not_watched = session.query(Movie)\
+            .filter_by(is_watched=False)\
+            .count()
+        return not_watched
+    def movie_count(self):
+        count = session.query(Movie).count()
+        return count
---
+++ services/movie_services.py
@@ -0,0 +1,75 @@
+from database import session
+from models import Movie
+from sqlalchemy import func
+
+class MovieServices:
+    def add_movie(self,
+                  title,
+                  genre,
+                  director,
+                  year,
+                  done,
+                  rate):
+        movie = session.query(Movie).filter_by(title = title, year = year , director = director).first()
+        if movie:
+            return False
+        movie = Movie(title=title,
+                     genre=genre,
+                     director=director,
+                     year=year,
+                     done=done,
+                     rate=rate
+                     )
+        session.add(movie)
+        session.commit()
+        return True
+    def delete_book(self,
+                    title,
+                    year,
+                    director):
+        movie = session.query(Movie).filter_by(title = title, year = year , director = director).first()
+        if not movie:
+            return False
+        session.delete(movie)
+        session.commit()
+        return True
+    def search_book(self,
+                    title,
+                    year):
+        movie = session.query(Movie).filter_by(title = title, year = year).first()
+        if not movie:
+            return False
+        return movie
+    def update_movie(self,
+                     title,
+                     year,
+                     director,
+                     new_title,
+                     new_year,
+                     new_director,
+                     new_rate
+                     ):
+        movie = session.query(Movie).filter_by(title = title, year = year,director=director).first()
+        if not movie:
+            return False
+        if new_title is not None:
+            movie.title = new_title
+        if new_director is not None:
+            movie.director = new_director
+        if new_year is not None:
+            movie.year = new_year
+        if new_rate is not None:
+            movie.rating = new_rate
+        session.commit()
+        return True
+    def search_movie_by_genre(self,
+                              genre):
+        movie = session.query(Movie).filter_by(genre = genre).all()
+        if not movie:
+            return False
+        return movie
+    
+        
+    
+    
+           
